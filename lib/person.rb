9513f5339e23454bf65a0a735bd5e66bd1d87edc
tuckyeah <h.rosebrock@gmail.com>
2016-09-16 15:30:17 -0400

    update person.rb with comments

--- lib/person.rb
+++ lib/person.rb
@@ -9,18 +9,20 @@ class Person
   def status=(new_status)
     @status = new_status
 
-    # Side Effect of changing a Person's status is to say it
-    %x{ say "Status changed to #{status}" }
+    # Side Effect of changing a Person's status is to say it (outloud)
+    # it runs this command in the terminal
+    %x{ say `Status changed to #{status}` }
   end
 
   def log_in
-    self.status = 'online'
+    self.status = 'online' # this is calling the 'status=' method from ln 14
   end
 
   def log_out
     # Problem:
-    # Will not call the setter method to and notify, or say, that the status
-    # has changed!
-    @status = 'offline'
+    # Will not call the setter method, nor say, that the status
+    # has changed! >> Because of the instance variable! Line 18 will not execute
+    # @status = 'offline' << get rid of this
+    self.status = 'offline' # right answer
   end
 end
